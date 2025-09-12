3a9d44c1c5a37e68ea9efaa93471d2af528490a0
Dimitar <dimitar.mandarliev@gmail.com>
2025-09-12 16:45:11 +0300

    feat: add core-js and regenerator-runtime for polyfilling and modern JS features

--- script.js
+++ script.js
@@ -88,6 +88,26 @@ state.user.loggedIn = false;
 console.log(stateClone);
 console.log(stateDeepClone);
 
-if (module.hot) {
-  module.hot.accept();
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
 }
+
+const dimitar = new Person('Dimitar');
+console.log('dimitar' ?? null);
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime';
