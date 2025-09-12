a18f3f018b7b4e4542c896a975e08ab249cee1e8
Dimitar <dimitar.mandarliev@gmail.com>
2025-09-12 15:55:58 +0300

    chore: initialize project with package.json, basic dependencies & .gitignore

--- script.js
+++ script.js
@@ -1,63 +1,93 @@
 // Importing module
 console.log('Importing module');
 
-// addtoCart('bread', 5);
-// console.log(price, tq);
+// // addtoCart('bread', 5);
+// // console.log(price, tq);
 
-// import * as ShoppingCart from './shoppingCart.js';
+// // import * as ShoppingCart from './shoppingCart.js';
 
-// ShoppingCart.addtoCart('bread', 5);
-// console.log(ShoppingCart.totalPrice);
+// // ShoppingCart.addtoCart('bread', 5);
+// // console.log(ShoppingCart.totalPrice);
 
-import add, { addtoCart, cart } from './shoppingCart.js'; // this imports the default export
+import add, { cart } from './shoppingCart.js'; // this imports the default export
 add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
 
-// console.log('Start fetching');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
+// // console.log('Start fetching');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
 
-// console.log('Something');
+// // console.log('Something');
 
-const getLastPost = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
+// const getLastPost = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// // Not very clean
+// // lastPost.then(last => console.log(last));
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const orderStock = (product, quantity) => {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+
+//   // Object that contains everything we want to make public here
+//   return {
+//     addtoCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart2.addtoCart('apple', 4);
+// ShoppingCart2.addtoCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost); // closure
+
+// Export
+// export.addtoCart = (product, quantity) => {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+
+// Import
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
 };
 
-const lastPost = getLastPost();
-console.log(lastPost);
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
 
-// Not very clean
-// lastPost.then(last => console.log(last));
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
-
-  const orderStock = (product, quantity) => {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
-
-  return {
-    addtoCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
-
-ShoppingCart2.addtoCart('apple', 4);
-ShoppingCart2.addtoCart('pizza', 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost); // closure
+if (module.hot) {
+  module.hot.accept();
+}
